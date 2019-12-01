bc6ecd2996eca8a0196f24e6d328bfe693f2fd54
Nikunj Raval <polymath.raval@gmail.com>
2019-12-01 15:14:18 +0530

    second week 2 solutions

--- week2_decomposition2/1_acyclicity/Acyclicity.java
+++ week2_decomposition2/1_acyclicity/Acyclicity.java
@@ -1,10 +1,63 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Acyclicity {
     private static int acyclic(ArrayList<Integer>[] adj) {
-        //write your code here
+        for( int vertex = 0; vertex < adj.length; vertex++ ) {
+            boolean[] visited = new boolean[adj.length];
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(vertex);
+            visited[vertex] = true;
+            while( !queue.isEmpty() ) {
+                int current = queue.remove();
+                for( int neighbour : adj[current] ) {
+                    if(neighbour == vertex) {
+                        return 1;
+                    }
+                    if( !visited[neighbour] ) {
+                        visited[neighbour] = true;
+                        queue.add(neighbour);
+                    }
+                }
+            }
+        }
         return 0;
+    }
+
+    private static int acyclic1(ArrayList<Integer>[] adj) {
+        int[] indegree = new int[adj.length];
+
+        // Compute indegrees
+        for(int vertex = 0; vertex < adj.length; vertex++) {
+            for(int neighbour: adj[vertex]) {
+                indegree[neighbour]++;
+            }
+        }
+
+        // Findout vertex with indegree of zero
+        Queue<Integer> queue = new LinkedList<>();
+        ArrayList<Integer> order = new ArrayList<>();
+        for(int vertex = 0; vertex < adj.length; vertex++) {
+            if(indegree[vertex] == 0)
+                queue.add(vertex);
+        }
+        while( !queue.isEmpty() ) {
+            int current = queue.remove();
+            order.add(current);
+            for(int neighbour : adj[current]) {
+                indegree[neighbour]--;
+                if(indegree[neighbour] == 0) {
+                    queue.add(neighbour);
+                }
+            }
+        }
+
+        if(order.size() == adj.length)
+            return 0;
+        else
+            return 1;
     }
 
     public static void main(String[] args) {
@@ -21,7 +74,7 @@ public class Acyclicity {
             y = scanner.nextInt();
             adj[x - 1].add(y - 1);
         }
-        System.out.println(acyclic(adj));
+        System.out.println(acyclic1(adj));
     }
 }
 
--- week2_decomposition2/2_toposort/Toposort.java
+++ week2_decomposition2/2_toposort/Toposort.java
@@ -1,17 +1,71 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Toposort {
     private static ArrayList<Integer> toposort(ArrayList<Integer>[] adj) {
-        int used[] = new int[adj.length];
+        boolean used[] = new boolean[adj.length];
         ArrayList<Integer> order = new ArrayList<Integer>();
-        //write your code here
+        for(int s = 0; s < adj.length; s++) {
+            dfs(adj, used, order, s);
+        }
+        //System.out.println(order);
+
+        for(int i = 0; i < order.size() / 2; i++) {
+            int temp = order.get(i);
+            order.set(i, order.get(order.size() - 1 - i));
+            order.set(order.size() - 1 - i, temp);
+        }
+        //System.out.println(order);
+
         return order;
     }
 
-    private static void dfs(ArrayList<Integer>[] adj, int[] used, ArrayList<Integer> order, int s) {
-      //write your code here
+    private static void dfs(ArrayList<Integer>[] adj, boolean[] used, ArrayList<Integer> order, int s) {
+        if( !used[s] ) {
+            for(int neighbour : adj[s]) {
+                dfs(adj, used, order, neighbour);
+            }
+            used[s] = true;
+            order.add(s);
+            //System.out.println(order);
+        }
+    }
+
+    private static ArrayList<Integer> toposort1(ArrayList<Integer>[] adj) {
+        int used[] = new int[adj.length];
+        ArrayList<Integer> order = new ArrayList<>();
+
+        // Compute the indegree matrix
+        int[] inDegree = new int[adj.length];
+        for(int vertex = 0; vertex < adj.length; vertex++) {
+            for(int neighbour : adj[vertex]) {
+                inDegree[neighbour]++;
+            }
+        }
+
+        // Get all elements where in the the indegree is zero
+        Queue<Integer> queue = new LinkedList<>();
+        for(int vertex = 0; vertex < adj.length; vertex++) {
+            if(inDegree[vertex] == 0) {
+                queue.add(vertex);
+            }
+        }
+
+        while( !queue.isEmpty() ) {
+            int vertex = queue.remove();
+            order.add(vertex);
+            for(int neighbour : adj[vertex]) {
+                inDegree[neighbour]--;
+                if(inDegree[neighbour] == 0) {
+                    queue.add(neighbour);
+                }
+            }
+        }
+
+        return order;
     }
 
     public static void main(String[] args) {
